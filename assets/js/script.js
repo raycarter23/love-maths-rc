a3c32f9f00ab38bb3f854a93772343b31b23d1f4
Ray Carter <raycarter158@gmail.com>
2024-10-17 16:00:06 +0000

    clear

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -85,6 +87,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -133,3 +137,17 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 
 }
+
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
+}
+
+/**
+ * Add gameType check to runGame()
+ * Create the display function / 
+ * Modify the calculateCorrectAnswer function / 
+ * We want the answer to be an integar a whole number -> one way to do this is to multiply our two operands together to form one side of the equation 
+ */
